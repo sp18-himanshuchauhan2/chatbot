3b8f0cdbc953ddf1a92e86da4340f47ce8a289a8
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-20 15:49:22 +0530

    created main function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from chat import app
 
-if __name__ == "__main__":
+def main():
+    print("\n********** Welcome to Flask App **********")
     print("Starting Flask Dashboard...")
     print("Available on http://127.0.0.1:5000")
     app.run(debug=True)
+
+if __name__ == "__main__":
+    main()
